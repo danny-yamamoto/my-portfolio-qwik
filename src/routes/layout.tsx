3879b2e3d51374f1a1940070e108eb107404704a
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-08-17 10:08:30 +0000

    Add Article Section.

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,6 +2,34 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
 import Footer from "../components/footer/footer";
 import styles from "./styles.css?inline";
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+type QiitaItem = {
+  title: string;
+  url: string;
+  id: string;
+};
+
+export const userArticles = routeLoader$(async () => {
+//  return "hoge";
+/*
+  const url = "https://qiita.com/api/v2/users/daisuke-yamamoto/items?page=1&per_page=10";
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+*/
+
+  const response = await fetch(`https://qiita.com/api/v2/users/daisuke-yamamoto/items?page=1&per_page=10`);
+  const qiitaItems: any[] = await response.json();
+  const filterQiitaItems: QiitaItem[] = qiitaItems.map(item => ({
+    title: item.title,
+    url: item.url,
+    id: item.id,
+  }));
+  return filterQiitaItems;
+
+});
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -15,6 +43,8 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
 };
 
 export default component$(() => {
+  const signal = userArticles();
+  console.log(signal);
   useStyles$(styles);
   return (
     <>
